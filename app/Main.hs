b9c880ffeae0a7285b759caf1befda8e2c3985f0
RuiC10 <ruicarvalho2000@gmail.com>
2024-01-25 14:10:20 +0000

    Added constraints to the user input when using the UI

--- app/Main.hs
+++ app/Main.hs
@@ -14,4 +14,7 @@ main = do
         Just bridge <- loadJuicyPNG "assets/Bridge.png"
         Just lantern <- loadJuicyPNG "assets/Lantern.png"
         [steps, maxTime] <- map (read :: String -> Int) <$> getArgs
-        play window background fr (initialState (steps, maxTime) bridge lantern) render keyHandler moveAdvs
+        if steps > 8 || steps < 0 || maxTime < 0 then
+            putStrLn "The number of steps should be between 1 and 7 and the maximum time should be a positive number" 
+        else
+            play window background fr (initialState (steps, maxTime) bridge lantern) render keyHandler moveAdvs
